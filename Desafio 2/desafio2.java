36a3f12eeae93bbd5f3e7d7e0cd70a3de188ac6d
unknown <lindomarjuniorsete@gmail.com>
2022-02-16 23:44:11 -0300

    second commit

---
+++ Desafio 2/desafio2.java
@@ -0,0 +1,32 @@
+package desafio2;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DesafioDois {
+    public static void main(String[] args) {
+        String senha;
+		int caracteres;
+
+		Scanner leia = new Scanner(System.in);
+
+		System.out.println("Digite sua senha: ");
+		senha = leia.next();
+		
+		String regex = "^(?=.*[0-9])"
+                + "(?=.*[a-z])(?=.*[A-Z])"
+                + "(?=.*[!@#$%^&*()-+])"
+                + "(?=\\S+$).{6}$";
+		
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(senha);
+		
+        if(senha.length()<6) {
+			   caracteres = 6 - senha.length();
+			   System.out.println("� necess�rio mais " + caracteres + " caracters!");
+		   }
+		
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+DESAFIO DE PROGRAMAÇÃO - ACADEMIA CAPGEMINI
+
+Tecnologias utilizadas:
+Ferramenta: Eclipse (IDE)
+Java como linguagem de programação
+Git para versionamento
+Github para hospedagem
+
+Como rodar a aplicação:
+
+1º Crie uma pasta nos seus documentos
+
+2º Com o botão direito do mouse, clique na opção git bash Here
+
+3º digite: git clone https://github.com/LindomarJunior7/desafio-capgemini
+
+4º Abra sua IDE e import a pasta criada para o repositório que acabou de ser clonado
+
+5º Clique em rodar o programa e digite o que lhe for solicitado no terminal
+
+6º Tudo certo! Agora você já sabe como criar uma escada em código, como proteger bem sua senha e quantos anagramas existem em um texto qualquer.
+
